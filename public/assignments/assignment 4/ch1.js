e71e68e5273fa6fa370fbe85a0952ac9a953ae36
Umair Abdullah <umairabdullah2512@gmail.com>
2020-06-07 16:11:13 +0500

    JS assignment

---
+++ public/assignments/assignment 4/ch1.js
@@ -0,0 +1,19 @@
+function task1(){
+    alert("Assalam o Alaikum");
+}
+function task2(){
+    alert("Error! Please enter a valid Password.");
+}
+
+function task3(){
+    alert("welcome to JS Land.. \nHappy Coding!");
+}
+function task4(){
+    alert("welcome to JS Land..")
+    alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+}
+
+function task5(){
+    console.log(alert("Hello... I can run JS through my web browser's console"));
+}
+
---
+++ public/assignments/assignment 4/ch2.js
@@ -0,0 +1,45 @@
+function task2_1(){
+    
+
+var username = "Umair";
+var myName ="Abdullah";
+
+var fulName = username + " " + myName;
+console.log(fulName)
+}
+
+function task2_2(){
+    var message = "Hello World";
+    alert(message)
+
+}
+function task2_3(){
+var fname=prompt("Enter Your First Name :");
+var lname=prompt("Enter Your Last Name :");
+
+var age=prompt("Enter Your Age :");
+
+alert(fname  + " "+ lname);
+alert(age+" "+"Years old");
+alert("Certified Mobail Applicatin Development")
+}
+
+
+function task2_4() {
+var a="PIZZA";
+var x="";
+for (i=0;i<a.length;i++){
+    a.pop()
+alert(a)
+
+
+}
+    
+}
+function task2_5() {
+    var umail=prompt("Enter Your Mail :");
+    alert("My Email Address IS "+ umail);
+
+
+    
+}
